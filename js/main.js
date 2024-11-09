2caeb551b44cac1a10459cfde38f8bd70e8e0f0b
Paulo Vieira <paulo_vieira22@hotmail.com>
2024-11-09 16:45:28 -0300

    envio dos arquivos

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
-})
-
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
-
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+$('#telefone').mask('(00) 00000-0000', { placeholder: '(DDD) 12345-6789' });
+$('#cpf').mask('000.000.000-00', { placeholder: '123.456.789-00' });
+$('#cep').mask('00000-000', { placeholder: '01234-567' });
 
 $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true,
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true,
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
         },
+    },
+    messages: {
+        nome: "Por favor, insira seu nome",
+        email: {
+            required: "Por favor, insira seu email",
+            email: "Por favor, insira um email válido"
+        },
+        telefone: "Por favor, insira seu telefone",
+        endereco: "Por favor, insira seu endereço",
+        cep: "Por favor, insira seu CEP",
+        cpf: "Por favor, insira seu CPF"
     },
     submitHandler: function (form) {
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
@@ -39,4 +42,4 @@ $('form').validate({
     invalidHandler: function (form, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
-})
+});
